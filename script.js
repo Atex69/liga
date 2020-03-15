c372415f7d63fe4f78805fc8ac20c553521db367
Emil Bayanov <atex-69@mail.ru>
2020-03-16 01:50:24 +0300

    верстка+адаптив+сортировка

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-$(document).ready(function () {
+let minPrice = 0;
+let maxPrice = 600;
+let sortBy = "noSort";
 
+$(document).ready(function () {
 
     $(window).scroll(function () {
         let height = $(window).scrollTop();
@@ -11,19 +14,20 @@ $(document).ready(function () {
         }
     });
 
-
     $('.mobile-menu').on('click', function (e) {
         e.preventDefault();
         $('.menu-btn').toggleClass('menu-active');
         $('.mobile-menu').toggleClass('menu-active');
         $('.mobile-menu__menu-header-wrap').toggleClass('menu-active');
     });
+
     $("ul").click(function () {
         $('.menu-btn').not(this).removeClass('menu-active');
         $('.mobile-menu').not(this).removeClass('menu-active');
         $('.mobile-menu__menu-header-wrap').not(this).removeClass('menu-active');
 
     });
+
     $("button").click(function () {
         $('.menu-btn').not(this).removeClass('menu-active');
         $('.mobile-menu').not(this).removeClass('menu-active');
@@ -31,40 +35,38 @@ $(document).ready(function () {
 
     });
 
-    function mySort() {
 
-        $('#CatCards.catHouseContentGridRightCards div.catCard[data-price]').sort(function (a, b) {
-            return parseInt($(a).attr('data-price'), 10) < parseInt($(b).attr('data-price'), 10) ? -1 : 1;
-        }).appendTo('div#CatCards.catHouseContentGridRightCards');
-    }
+    $('#showCatsButton').on("click", showCats);
 
-    mySort()
+    $('#filter').on("change", function() {
+        sortBy = $("#filter").val();
+        showCats();
+    });
 
+    showCats();
 });
 
-
-
-$('#showCatsButton').click(function () {
+function showCats() {
     removeCards();
     filter();
 
-    let arrCat=$('#catCheckContainer input:checkbox:checked').map(function() {return this.value;}).get();
+    let arrCat = $('#catCheckContainer input:checkbox:checked').map(function () {
+        return this.value;
+    }).get();
     console.log(arrCat);
-});
+}
 
 function removeCards() {
     $("div.catHouseContentGridRightCards").empty()
 }
 
 function filter() {
-    let minPrice = $('.catHouseMinPrice').val();
-    let area1  = $('#area1');
-    let area2  = $('#area2');
-    let area3  = $('#area3');
-    let area4  = $('#area4');
-    let area5  = $('#area5');
-    let area6  = $('#area6');
-    let maxPrice = $('.catHouseMaxPrice').val();
+    minPrice = $('.catHouseMinPrice').val();
+    if (minPrice === "")
+        minPrice = 0;
+    maxPrice = $('.catHouseMaxPrice').val();
+    if (maxPrice === "")
+        maxPrice = 600;
     render(minPrice, maxPrice,  'area');
 }
 
@@ -83,7 +85,7 @@ class ModelCat {
 
 
 let cats = [
-    new ModelCat('Эконом', 'img/catHouseContentGridRightCardEconom.jpg', 100, '9070180', '0.63', '<img src="img/vector.svg">'),
+    new ModelCat('Эконом', 'img/catHouseContentGridRightCardEconom.jpg', 100, '9070180', '0.63', '<img src="img/Vector.svg">'),
     new ModelCat('Эконом плюс', 'img/catHouseContentGridRightCardEconomPlus.jpg', 200, '90100180', '0.90', '<img src="img/vector4.png"><img src="img/vector3.png">'),
     new ModelCat('Комфорт', 'img/catHouseContentGridRightCardComfort.jpg', 250, '100125180', '1.13', '<img src="img/vector4.png"><img src="img/vector3.png"><img src="img/vector2.png">'),
     new ModelCat('Сьют', 'img/catHouseContentGridRightCardSuite.jpg', 350, '125125180', '1.56', '<img src="img/vector4.png"><img src="img/vector3.png"><img src="img/vector2.png">'),
@@ -92,52 +94,19 @@ let cats = [
 ];
 
 
-let templateCat = (a) => `<div>
+let templateCat = (a) => `<div class="catCard">
 <div class="catCardImg"><img src="${a.image}"></div>
 <div class="catCardName">${a.name}</div>
 <div class="catCardSize">Размеры (ШхГхВ) - ${a.size}см</div>
 <div class="catCardArea">Площадь - ${a.area}м2</div>
 <div class="catCardEquip">Оснащение номера ${a.equip}</div>
 <div class="catCardPrice">Цена за сутки:<bold>${a.price}</bold></div>
-  <a href="#"> <button class="catCardButton"><p>Забронировать</p> <img src="img/paw.png"> </button></a>
+  <a href="#"> <button class="catCardButton"><p>Забронировать</p> <img src="img/paw.png" alt="paw"> </button></a>
 </div>`;
 
 let filterByPrice = (minPrice, maxPrice) => {
     return (cat) => cat.price >= minPrice && cat.price <= maxPrice;
 };
-
- let filterBycheckArea1 = (area1) => {
-     return (cat) => area1 && cat.area === '0.63';
- };
-
- let filterBycheckArea2 = (area2) => {
-     return (cat) => area2 && cat.area === '0.90';
- };
-
- let filterBycheckArea3 = (area3) => {
-     return (cat) => area3 && cat.area === '1.13';
- };
-
- let filterBycheckArea4 = (area4) => {
-     return (cat) => area4 && cat.area === '1.56';
- };
-
- let filterBycheckArea5 = (area5) => {
-     return (cat) => area5 && cat.area === '2.56';
- };
-
- let filterBycheckArea6 = (area6) => {
-     return (cat) => area6 && cat.area === '2.88';
- };
-
-
-
-let catSelect;
-document.getElementById('filter').value= catSelect;
-
-if (catSelect == 'areasUp'){
-
-}
 
 let sortByAreaUp = function (a, b) {
     if (a.area > b.area) {
@@ -149,10 +118,6 @@ let sortByAreaUp = function (a, b) {
 
     return 0;
 };
-
-if (catSelect == 'areasDown'){
-
-}
 
 let sortByAreaDown = function (a, b) {
     if (a.area < b.area) {
@@ -166,10 +131,6 @@ let sortByAreaDown = function (a, b) {
     return 0;
 };
 
-if (catSelect == 'priceUp'){
-
-}
-
 let sortByPriceUp = function (a, b) {
     if (a.price > b.price) {
         return 1;
@@ -181,10 +142,6 @@ let sortByPriceUp = function (a, b) {
     return 0;
 };
 
-if (catSelect == 'priceDown'){
-
-}
-
 
 let sortByPriceDown = function (a, b) {
     if (a.price < b.price) {
@@ -194,30 +151,28 @@ let sortByPriceDown = function (a, b) {
         return -1;
     }
 
-
     return 0;
 };
 
-let render = (minPrice = 0, maxPrice = 9999, sortby) => {
-    let checkbox1 = true;
-
+function render() {
     let sortFunction;
-    if (sortby === 'area') {
+    if (sortBy === 'areasUp') {
         sortFunction = sortByAreaUp;
-    } else  if (sortby === 'area') {
+    } else if (sortBy === 'areasDown') {
         sortFunction = sortByAreaDown;
-    } else  if (sortby === 'price') {
+    } else if (sortBy === 'priceUp') {
         sortFunction = sortByPriceUp;
-    }else  if (sortby === 'price') {
+    } else if (sortBy === 'priceDown') {
         sortFunction = sortByPriceDown;
     }
 
+    console.log(sortBy);
+
     let htmlCats = cats
         .filter(filterByPrice(minPrice, maxPrice))
-
         .sort(sortFunction)
         .map(cat => templateCat(cat));
     let $catHouseContentGridRightCards = $('.catHouseContentGridRightCards');
     $catHouseContentGridRightCards.empty();
-    $catHouseContentGridRightCards.html(htmlCats)
-};
+    $catHouseContentGridRightCards.html(htmlCats);
+}
